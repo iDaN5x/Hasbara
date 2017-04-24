1a5af16bd4029b1c194ec9e8653b426794af3b27
Idan Asraf <idan.asraf@sap.com>
2017-04-24 13:38:36 +0300

    Starting work

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const Http = require('http'),
       Path = require('path');
 
 // External dependencies.
-const Express = require('express'),
-      Twitter = require('twitter'),
+const Twitter = require('twit'),
+      lodash = require('lodash'),
+      Express = require('express'),
       SocketIO = require('socket.io'),
-      RethinkDB = require('rethinkdbdash');
+      RethinkDB = require('rethinkdbdash'),
+      NodeGeocoder = require('node-geocoder');
 
 // Load configuration file.
-const config = require('./config');
+const config = require('./config.json');
 
 // Declare app components.
 let app = new Express(),
@@ -17,14 +19,24 @@ let app = new Express(),
     io = new SocketIO(server);
 
 // Connect to database.
-let r = new RethinkDB(config.rethinkdb);
+//let r = new RethinkDB(config.rethinkdb);
 
 // Expose static files to client.
 app.use(Express.static(Path.join(__dirname, 'public')));
 
+// Connect to Twitter API.
+var twitter = new Twitter(config.twitter);
+
+// Open tweet stream.
+var stream = twitter.stream('statuses/filter', config.stream);
+
+stream.on('tweet', tweet => {
+  console.log(tweet.user.location);
+});
+
 // Start listening for incoming requests.
 let port = config.port || 3000;
 
-http.listen(port, () => {
+server.listen(port, () => {
   console.log(`Hasbara app started listening on port ${port}`);
 });
